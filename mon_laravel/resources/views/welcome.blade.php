0363a45157ad0224a936f836ec57795470123051
bilalbouherrafa <bilal.bouherrafa@yahoo.com>
2017-12-06 06:11:33 -0800

    page connexion

--- mon_laravel/resources/views/welcome.blade.php
+++ mon_laravel/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel test</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -46,7 +46,8 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 104px;
+
             }
 
             .links > a {
@@ -58,7 +59,14 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
+	    p{
+		color: #636b6f;
+		font-size: 12px;
+                font-weight: 600;
+		letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+	    }
             .m-b-md {
                 margin-bottom: 30px;
             }
@@ -79,15 +87,20 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test
+                    Application Parking
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+  		    <h1> Connexion </h1>
+		    <p> Utilisateur  </p>
+		    <input type="text">
+		    <br>
+		    <br>
+		    <p> Mot de passe </p>
+		    <input type="password">
+		    <br>
+		    <br>
+		    <input type="submit" name="nom" value="Valider">
                 </div>
             </div>
         </div>
